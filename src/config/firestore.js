6371b9f0e45b1640c9ae6bba7e5f1cb09b0d5631
ahmad-ausaf0 <ahmadausaf0@gmail.com>
2024-08-22 16:20:58 +0530

    change

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -31,12 +31,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD4gYRSDNFwkXq9RHObmLa3Zrgsp_NH3v0",
-  authDomain: "employee-database-7ce77.firebaseapp.com",
-  projectId: "employee-database-7ce77",
-  storageBucket: "employee-database-7ce77.appspot.com",
-  messagingSenderId: "747332903112",
-  appId: "1:747332903112:web:75fa9e1807a72eb03a45fd"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
